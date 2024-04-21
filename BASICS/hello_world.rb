376b66f491fde177213ca17d9c710e62e0ee2df1
Nimira43 <n.rabone72@aol.com>
2024-04-21 20:11:11 +0100

    Updated

--- BASICS/hello_world.rb
+++ BASICS/hello_world.rb
@@ -1,1 +1,10 @@
 puts "Hello World"
+puts 1 + 2
+puts 134.7767
+puts
+puts "line break"
+puts 5, "hi", 67, 6.98, "cat"
+puts
+puts
+puts
+puts "This line \nis going \nto be \nput on \ndifferent lines"
