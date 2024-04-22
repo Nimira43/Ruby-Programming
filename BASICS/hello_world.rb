63a6b885a30b60207420c9528d1a2696115ab1b2
Nimira43 <n.rabone72@aol.com>
2024-04-22 20:58:41 +0100

    Updated

--- BASICS/hello_world.rb
+++ BASICS/hello_world.rb
@@ -10,3 +10,21 @@ puts
 puts "This line \nis going \nto be \nput on \ndifferent lines"
 puts "This line \tis going \tto be \ttabbed."
 puts "This line \tis going \tto be a\t 'quote.'"
+puts "This line \tis going \tto be a\t \"double quote.\""
+
+print "Hello there"
+print "Hi"
+
+puts "Puts puts a line break"
+puts "after each puts statement"
+
+print "Print however"
+print "doesn't add a line break"
+
+puts
+puts "I just added an empty puts above"
+puts "and it provided a line break after the preceding print statement"
+print "Now using print"
+print " which is annoying because I have to include"
+print " a space either at the start or the end for "
+print "this statement to appear on one line with spaces."
