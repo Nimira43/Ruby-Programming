889fdadfb02fb24f56e38725727504c1c0624764
Nimira43 <n.rabone72@aol.com>
2024-05-18 08:36:55 +0100

    Variables lecture

--- VARIABLES/variables.rb
+++ VARIABLES/variables.rb
@@ -1,1 +1,37 @@
 # variables
+ 
+# What is a variable?
+#  A variable is a name we assign to an object in our
+#  program. A variable is a reference to an object.
+#  Variables are sometimes called pointers or identifiers
+#  because they “point to” or “identify” objects.
+#  The variable itself is NOT an object. It is a placeholder
+#  or name for an object
+
+pet = "cat"
+age = 7
+name = "Lenny"
+
+puts pet
+puts age
+puts name
+
+formula_1_champion_1988 = "Ayrton Senna"
+puts formula_1_champion_1988
+
+num_1 = 1972
+puts num_1 + 52
+
+puts pet + name
+
+#puts "My " + pet + " is called " + name + ". He is " + age + " years old."
+
+thing = 45
+puts thing
+thing = "spanner"
+puts thing
+thing = 45.45
+puts thing
+
+thing = thing + 32
+puts thing
