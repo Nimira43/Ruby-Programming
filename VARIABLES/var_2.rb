19dfe80c1964066456aff3a5c0570c76d642beba
Nimira43 <n.rabone72@aol.com>
2024-09-24 08:44:43 +0100

    More on variables.

---
+++ VARIABLES/var_2.rb
@@ -0,0 +1,23 @@
+###################################
+### Exceptions
+## NameError
+
+# 1 - food variable not declared
+#puts food
+
+# 2 - typos
+#food = "steak"
+#puts foood
+
+###################################
+### Parallel Variable Assignment
+
+# Normal way
+a = 10
+b = 15
+c = 20
+puts  a, b, c
+
+# Shortcut
+a, b, c = 10, 15, 20
+puts a, b, c
