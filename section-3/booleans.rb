ca5b08d82e4a28f763b6aa0b56d4e8a877ec0d55
Nimira43 <n.rabone72@aol.com>
2025-10-03 20:58:34 +0100

    Comparing integer to floats

--- section-3/booleans.rb
+++ section-3/booleans.rb
@@ -31,6 +31,12 @@ puts "5" == 5.to_s        # true - both strings in this comparison
 puts
 
 num1 = 5
-puts num1.to_s.class            # string
-puts num1 == "5"                # false - not in this comparison. num1 is still an integer
-puts num1.to_s. == "5"     # true - in this comparison they are the same
+puts num1.to_s.class      # string
+puts num1 == "5"          # false - not in this comparison. num1 is still an integer
+puts num1.to_s. == "5"    # true - in this comparison they are the same
+
+puts
+
+puts 5.class             # integer
+puts 5.0.class           # float
+puts 5 == 5.0            # true - different classes but both are numeric
