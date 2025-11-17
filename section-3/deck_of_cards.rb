f25dd0d572d5989c26a3c1a8dc3d402bd0199eac
Nimira43 <n.rabone72@aol.com>
2025-11-17 15:45:23 +0000

    Looping over suits and values and creating cards.

--- section-3/deck_of_cards.rb
+++ section-3/deck_of_cards.rb
@@ -5,5 +5,11 @@ class Deck
     @cards = []
     suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+  
+    suits.each do |suit|
+      values.each do |value|
+        @cards << "#{value} of #{suit}"
+      end
+    end
   end
 end
