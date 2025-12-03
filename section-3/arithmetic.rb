540aaff8135fe98029f78d91e0431f6c6476039a
Nimira43 <n.rabone72@aol.com>
2025-12-03 11:20:49 +0000

    floor - returns integer

--- section-3/arithmetic.rb
+++ section-3/arithmetic.rb
@@ -3,7 +3,9 @@ puts "3 - 5 is: #{3.0 - 5}"
 puts "3 * 5 is: #{3.0 * 5}"
 puts "3 / 5 is: #{3.0 / 5}"
 
-# +, -, *, and / are actually methods defined on those objects
+puts
+
+puts "+, -, *, and / are actually methods defined on those objects"
 puts 5.+(3)
 puts 5.-(3)
 puts 5.*(3)
@@ -11,7 +13,12 @@ puts 5./(3)         # = 1
 puts 5.0./(3)       # = 1.6666666666666667
 puts 5.div(3)       # = 1
 
-div always returns an integer quotient, regardless of type.
+# div always returns an integer quotient, regardless of type.
 puts 5.0.div(3)     # = 1
+puts 5.0.div(3.0)   # = 1
+
+puts "Float Methods"
+puts 10.5.floor
+puts 10.5.floor.class
 
 
