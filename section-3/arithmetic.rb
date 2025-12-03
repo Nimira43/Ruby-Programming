fe698cee54185eec8053e382a05b93a4fb523f09
Nimira43 <n.rabone72@aol.com>
2025-12-03 10:58:59 +0000

    # +, -, *, and / are actually methods defined on those objects

--- section-3/arithmetic.rb
+++ section-3/arithmetic.rb
@@ -3,3 +3,9 @@ puts "3 - 5 is: #{3.0 - 5}"
 puts "3 * 5 is: #{3.0 * 5}"
 puts "3 / 5 is: #{3.0 / 5}"
 
+# +, -, *, and / are actually methods defined on those objects
+puts 5.+(3)
+puts 5.-(3)
+puts 5.*(3)
+puts 5./(3)
+
