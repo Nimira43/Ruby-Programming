a8e2ee4e4aef94d33e0306ec8d659bd7d75a40ab
Nimira43 <n.rabone72@aol.com>
2025-12-03 11:05:39 +0000

    div  always returns an integer quotient, regardless of type.

--- section-3/arithmetic.rb
+++ section-3/arithmetic.rb
@@ -7,5 +7,11 @@ puts "3 / 5 is: #{3.0 / 5}"
 puts 5.+(3)
 puts 5.-(3)
 puts 5.*(3)
-puts 5./(3)
+puts 5./(3)         # = 1
+puts 5.0./(3)       # = 1.6666666666666667
+puts 5.div(3)       # = 1
+
+div always returns an integer quotient, regardless of type.
+puts 5.0.div(3)     # = 1
+
 
