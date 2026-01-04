9db58a11c1de236203f8faaf457d3daa48aa16e4
Nimira43 <n.rabone72@aol.com>
2026-01-04 23:03:47 +0000

    Float methods

---
+++ section-4/float_methods.rb
@@ -0,0 +1,29 @@
+# Float Methods
+
+puts 10.5.floor
+puts 10.9.floor
+puts 10.9.floor.class
+
+puts
+
+puts 10.2.ceil
+puts 10.5.ceil
+puts 10.9.ceil
+puts 10.9.ceil.class
+
+puts
+
+puts 3.14159.round
+puts 3.54359.round
+puts 3.54359.round.class
+puts 3.14159.round(2)
+puts 3.14159.round(3)
+puts 3.17459.round(3)
+puts 3.17459.round(3).class
+
+puts
+
+puts 10.2.abs
+puts -10.5.abs
+puts 34.abs
+puts -34.abs
